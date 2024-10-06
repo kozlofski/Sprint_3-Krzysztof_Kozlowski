e3208ec10163f9a03f51513b217e41510fd3d113
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-10-06 15:44:27 +0200

    Task 03 finished

---
+++ task_03.js
@@ -0,0 +1,37 @@
+const number1 = 4;
+const number2 = 3;
+const operationType = '%';
+
+let result = 0;
+let message = ''
+let errorMessage = ''
+
+if(operationType === '+') {
+    result = number1 + number2;
+} else if(operationType === '-') {
+    result = number1 - number2;
+} else if(operationType === '*') {
+    result = number1 * number2;
+} else if(operationType === '/') {
+    if(number2 === 0) {
+        errorMessage = 'Nie można dzielić przez zero';
+    } else {
+        result = number1 / number2;
+    }
+} else if(operationType === '%') {
+    if(number2 === 0) {
+        errorMessage = 'Nie można dzielić modulo przez zero';
+    } else {
+        result = number1 % number2;
+    }
+} else {
+    errorMessage = 'Nieobsługiwany typ operacji';
+}
+
+if(errorMessage.length === 0) {
+    const evenOrOdd = (result % 2 === 0) ? 'parzysty' : 'nieparzysty';
+    message = `Wynik operacji \"${operationType}\" to: ${result}, który jest ${evenOrOdd}`;
+    console.log(message);
+} else {
+    console.log(errorMessage)
+}
